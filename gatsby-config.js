0964ba497ac7f06a9c099b76b8a79037e1697cab
Harry Saputra <flexnewbie@gmail.com>
2022-03-20 10:11:39 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Bolubutter.id`,
+    description: `Bake with love`,
+    author: `bolubutter.id`,
+    siteUrl: `https://bolubutter.id/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
